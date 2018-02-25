19061466e568db0d81bf9852c07e062ad7c11517
John Pham <johnphammail@gmail.com>
2018-02-25 08:01:34 -0800

    adding funding layout

--- pages/funding.vue
+++ pages/funding.vue
@@ -1,13 +1,18 @@
 <template>
-  <div class="people section py-4">
-    <h3 class="section-title text-center m-5">C-SAMBA Funding</h3>
-    <div class="container">
-      <p>
-        C-SAMBA is supported in part by research grants from the National Science Foundation.
-      </p>
-      <p>
-        Disclaimer: Any opinions, findings, and conclusions or recommendations expressed in this material are those of the author(s) and do not necessarily reflect the views of the National Science Foundation.
-      </p>
-    </div>
+  <div>
+    <h2 class="section-title-primary text-center">C-SAMBA Funding</h2>
+    <Hero :hero="'/img/hero/Hero-Misc.png'">
+      <strong>C-SAMBA</strong> is supported in part by research grants from the <strong>National Science Foundation</strong>.
+    </Hero>
   </div>
 </template>
+
+<script>
+import Hero from '../components/Hero-Small'
+
+export default {
+  components: {
+    Hero
+  }
+}
+</script>
